bdb582a2572f767d8686d0e35daa7e6287404e3a
akshat-application <akpapk11@gmail.com>
2024-07-12 13:55:03 +0530

    updated post api calls

--- src/main/java/com/example/demo/packageApi/controller/UserController.java
+++ src/main/java/com/example/demo/packageApi/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.demo.packageApi.controller;
 
 import com.example.demo.packageApi.model.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.service.UserService;
 
 import java.lang.module.ModuleDescriptor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,12 +24,8 @@ public class UserController {
     public UserController() {
         User user1 = new User(1, "Ida", 32, "ida@mail.com");
         User user2 = new User(2, "Hans", 26, "hans@mail.com");
-        User user3 = new User(3, "Lars", 45, "lars@mail.com");
-        User user4 = new User(4, "Ben", 32, "ben@mail.com");
-        User user5 = new User(5, "Eva", 59, "eva@mail.com");
-        userList.addAll(Arrays.asList(user1, user2, user3, user4, user5));
+        userList.addAll(Arrays.asList(user1, user2));
     }
-
 
     @GetMapping("/user")
     public User getUser(@RequestParam(value = "id", defaultValue = "World") int id) {
@@ -37,9 +37,12 @@ public class UserController {
         return new User(0, "null", 0, "null");
     }
 
-    @PostMapping("/data")
-    public Boolean storeData(@RequestBody() User user) {
+    @PostMapping(value = "/data")
+    public Boolean storeData(@RequestBody String data) {
 //        Optional<User> user = userService.getUser(id);
+//        userList.add(user);
+        Type listType = new TypeToken<User>() {}.getType();
+        User user = new Gson().fromJson(data, listType);
         userList.add(user);
         return true;
     }
